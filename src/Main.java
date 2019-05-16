d7e6fc24e4c2f50935cc86e17478d0f4710623a2
kwr2 <kwr2@st-andrews.ac.uk>
2019-05-16 16:39:03 +0100

    Testing input

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        System.out.println("hello world 1");
         Scanner scanner = new Scanner(System.in);
         String inputString = scanner.nextLine();
         System.out.println(inputString);
+        System.out.println("hello world 2");
     }
 }
