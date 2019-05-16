55f21372c60cd5aafb23e5bf9435294a4885deaf
kwr2 <kwr2@st-andrews.ac.uk>
2019-05-16 16:21:01 +0100

    added stacscheck

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 import java.io.IOException;
 
-public class SATtoThreeSAT {
+public class Main {
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
         //initial push
 
