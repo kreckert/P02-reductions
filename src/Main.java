3a3480c706faf46f7c7de1d7c27039e7487119b5
kwr2 <kwr2@st-andrews.ac.uk>
2019-05-16 16:25:26 +0100

    changed hello world

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! 2");
         //initial push
 
         int ch;
