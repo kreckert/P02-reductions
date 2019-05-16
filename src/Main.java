003d4cc3ab6d114c9399de5d055c761727634fd8
kwr2 <kwr2@st-andrews.ac.uk>
2019-05-16 16:37:00 +0100

    Testing input

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello World! 2");
-        //initial push
-
-        int ch;
-        while ((ch = System.in.read ()) != -1)
-            System.out.print ((char) ch);
-
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        System.out.println(inputString);
     }
 }
--- src/SATtoThreeSAT.java
+++ src/SATtoThreeSAT.java
@@ -1,6 +1,11 @@
-
-
 public class SATtoThreeSAT {
 
+    private boolean hasNext;
+    private boolean seenProblemLine;
+    private String line;
+
+    private void readLine() {
+
+    }
 
 }
