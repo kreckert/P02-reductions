a1feb7704549d22584270bd77c47ee221f64398a
kwr2 <kwr2@st-andrews.ac.uk>
2019-05-16 16:24:52 +0100

    created main

--- src/SATtoThreeSAT.java
+++ src/SATtoThreeSAT.java
@@ -1,14 +1,6 @@
-import java.io.IOException;
+
 
 public class SATtoThreeSAT {
 
-    public static void main(String[] args) throws IOException{
-        System.out.println("Hello World!");
-        //initial push
 
-        int ch;
-        while ((ch = System.in.read ()) != -1)
-            System.out.print ((char) ch);
-
-    }
 }
